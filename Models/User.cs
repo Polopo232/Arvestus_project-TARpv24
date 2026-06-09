e3adb04f7d9f0772ea4992909624daea53fb55aa
opilane <opilane@HP-CZC2349HT2>
2026-06-09 13:58:39 +0300

    новый класс для пользователей

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+
+namespace Arvestus_project_TARpv24.Models
+{
+    [Table("Users")]
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [Unique, NotNull]
+        public string Username { get; set; }
+
+        [NotNull]
+        public string Password { get; set; }
+
+        [NotNull]
+        public string Role { get; set; }
+    }
+}
